b8a4b9d45d02fb0b19129070f4087d3de8a041dd
pramodkumar115 <pramodkumar115@gmail.com>
2020-02-01 23:08:07 +0800

    First Commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,8 +1,3 @@
-# multiselect-react
-React Library for Multiselect dropdown
-
-Example
-```
 import React, { Component } from 'react'
 
 import MultiSelect from 'multiselect-react'
@@ -11,6 +6,9 @@ export default class App extends Component {
 
   handleChange = (e) => {
     console.log(e)
+  }
+  handleChange2  = (e) => {
+    console.log("2: ", e)
   }
   render() {
     const valueList = [{ id: 1, value: "One" },
@@ -22,15 +20,26 @@ export default class App extends Component {
     { id: 7, value: "Seven" },
     { id: 8, value: "Eight" },
     { id: 9, value: "Nine" },
+    { id: 10, value: "Ten" },
+    { id: 1, value: "One" },
+    { id: 2, value: "Two" },
+    { id: 3, value: "Three" },
+    { id: 4, value: "Four" },
+    { id: 5, value: "Five" },
+    { id: 6, value: "Six" },
+    { id: 7, value: "Seven" },
+    { id: 8, value: "Eight" },
+    { id: 9, value: "Nine" },
     { id: 10, value: "Ten" }]
     return (
+
       <div style={{ padding: "10px", width: "400px" }}>
-      <!-- By Default display field is considered to be value.-->
+        <input type="text"></input>
         <MultiSelect valueList={valueList} onChange={this.handleChange} />
-        <!-- If you have array of objects where you want to change the displayField, you can do so as in below snippet --> 
+        <input type="text"></input>
         <MultiSelect valueList={valueList} displayField="value" onChange={this.handleChange2} />
+        <div>Hello</div>
       </div>
     )
   }
 }
-```
---
+++ src/index.tsx
@@ -0,0 +1,156 @@
+/**
+ * @class MultiSelect
+ */
+
+import * as React from 'react'
+import styles from './styles.css'
+
+export type Props = { valueList: any[], displayField: string, onChange: Function }
+export type State = { items: any[], filterInput: string, 
+  selectedValues: Set<any>, currentFocus: number }
+export default class MultiSelect extends React.Component<Props, State> {
+  public static defaultProps = {
+    displayField: "value"
+};
+
+  private dropdownRef = React.createRef<HTMLDivElement>()
+  private wrapperRef = React.createRef<HTMLDivElement>()
+  private dropdownSearchRef = React.createRef<HTMLInputElement>()
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      items: this.props.valueList,
+      filterInput: "",
+      selectedValues: new Set<any>(),
+      currentFocus: -1
+    }
+  }
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+    document.addEventListener('keydown', this.handleKeyboardEvent);
+    document.addEventListener("focus", this.onFocus);
+    //document.addEventListener('keyup', this.handleClick)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+    document.removeEventListener('keydown', this.handleKeyboardEvent);
+    document.removeEventListener("focus", this.onFocus);
+  }
+  onFocus = (event: any) => {
+    console.log("On focus", event);
+  }
+  handleKeyboardEvent = (event: KeyboardEvent) => {
+    if (this.wrapperRef && (this.wrapperRef as any).current.contains(event.target)) {
+      let scrollTop = 0
+      if(this.dropdownRef && this.dropdownRef.current)
+      scrollTop = this.dropdownRef.current.scrollTop
+      if (event.keyCode == 40) {
+        this.state.currentFocus < this.state.items.length - 1 && this.setState({ currentFocus: this.state.currentFocus + 1 })
+        if(this.dropdownRef && this.dropdownRef.current && this.state.currentFocus >= 9)
+        this.dropdownRef.current.scrollTop = scrollTop + 53
+      }
+      else if (event.keyCode == 38) {
+        this.state.currentFocus > -1 && this.setState({ currentFocus: this.state.currentFocus - 1 })
+        if(this.dropdownRef && this.dropdownRef.current && this.state.currentFocus <= 9)
+        this.dropdownRef.current.scrollTop = scrollTop - 53
+      }
+      else if (event.keyCode == 27) {
+        const node = this.dropdownRef.current
+        node != null && node.classList.contains(styles.active) && node.classList.remove(styles.active)
+      }
+      else if (event.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        event.preventDefault();
+        if (this.state.currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          this.selectItem(this.state.items[this.state.currentFocus]);
+        }
+      }
+      this.handleClick()
+    } else {
+      this.handleClickOutside(event)
+    }
+  }
+  handleClick = () => {
+    const node = this.dropdownRef.current;
+    node != null && !node.classList.contains(styles.active) &&
+    node.classList.add(styles.active)
+    this.dropdownSearchRef && this.dropdownSearchRef.current 
+    && this.dropdownSearchRef.current.focus()
+  };
+  handleClickOutside = (event: any) => {
+    if (this.wrapperRef && !(this.wrapperRef as any).current.contains(event.target)) {
+      const node = this.dropdownRef.current
+      node != null && node.classList.contains(styles.active) && node.classList.remove(styles.active)
+    }
+  }
+  filterRecords = (e: React.KeyboardEvent | React.MouseEvent | React.ChangeEvent) => {
+    const value = ((e.target) as any).value
+    if (this.state.filterInput != value) {
+      this.setState({currentFocus: -1})
+      this.setState({
+        filterInput: value
+      }, () => {
+        if (this.state.filterInput != null) {
+          this.setState({
+            items: this.props.valueList.filter(v =>
+              (v[this.props.displayField] as string)
+                .toLowerCase()
+                .indexOf(this.state.filterInput.toLowerCase()) != -1)
+          })
+        } else {
+          this.setState({
+            items: this.props.valueList
+          })
+        }
+      })
+    }
+  }
+  selectItem = (selected: any) => {
+    const { selectedValues } = this.state;
+    selectedValues.add(selected)
+    this.setState({
+      selectedValues: selectedValues
+    }, this.props.onChange(Array.from(this.state.selectedValues)));
+    this.setState({currentFocus: -1})
+  }
+  removeItem = (selected: any) => {
+    const { selectedValues } = this.state;
+    selectedValues.delete(selected)
+    this.setState({
+      selectedValues: selectedValues
+    }, this.props.onChange(Array.from(this.state.selectedValues)));
+  }
+  render() {
+    return (
+      <div className={styles.multiSelect} ref={this.wrapperRef}>
+        <input style={{opacity: 0, position: "absolute"}}></input>
+        <div className={styles.multiSelectBox} onClick={this.handleClick} >
+          <div className={styles.multiSelectBoxContents}>
+            {this.state.selectedValues != null &&
+              Array.from(this.state.selectedValues).map((v, index) =>
+                <div className={styles.multiSelectBoxContentItem} key={`msv_${index}`}>
+                  <span>{v[this.props.displayField]}</span>
+                  <label style={{ cursor: "pointer" }} onClick={() => this.removeItem(v)}>&#9938;</label>
+                </div>)}
+          </div>
+          <span className={styles.multiSelectArrow}>&#11206;</span>
+        </div>
+        <div className={styles.multiSelectItems} ref={this.dropdownRef}>
+          <ul>
+            <li><input type="search" placeholder="Search" ref={this.dropdownSearchRef}
+              onKeyUp={this.filterRecords} onChange={this.filterRecords}/></li>
+            {this.state.items.map((v, key) =>
+              <li className={key === this.state.currentFocus ? styles.multiSelectItemfocus : ""}
+                key={`ms_${key}`} onClick={() => this.selectItem(v)}>
+                {v[this.props.displayField]}
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
